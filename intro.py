1e6fa191572805525bb08724e3b7960503d990a6
Ethan Mouritsen <ethan.mouritsen@tooeleschools.org>
2023-08-21 14:15:56 -0600

    first push

---
+++ intro.py
@@ -0,0 +1,22 @@
+firstName = "My first name is Ethan"
+middleName = "My middle name is Hale"
+lastName = "My last name is Mouritsen"
+age = "I am 16 years old"
+siblings = "I have 2 sisters"
+favoriteClass = "My favorite class this year is Python"
+favoriteGame = "My favorite game is Tears of the Kingdom"
+birthday = "My birthday is September 11th, 2006"
+numberOfParents = "I have two parents(crazy)"
+favoriteDayOfTheWeek = "My favorite day of the week is Saturday"
+
+print(firstName)
+print(middleName)
+print(lastName)
+print(age)
+print(siblings)
+print(favoriteClass)
+print(favoriteGame)
+print(birthday)
+print(numberOfParents)
+print(favoriteDayOfTheWeek)
+input()
